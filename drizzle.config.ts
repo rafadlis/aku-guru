37cb4d725a7a75559dd6ea1a4dc5bf6e3f3736ae
rafadlis <rahmatfadlis@gmail.com>
2024-12-14 18:25:04 +0700

    refactor(config): Simplify database connection configuration by replacing individual credentials with a single DATABASE_URL environment variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: parseInt(process.env.DB_PORT!),
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
+    url: process.env.DATABASE_URL!,
   },
 });
