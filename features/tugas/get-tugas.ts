6027f694473ca851b7da50d3f1edce7a2d7272ea
rafadlis <rahmatfadlis@gmail.com>
2024-12-14 18:31:16 +0700

    refactor(get-tugas): Standardize string quotes for consistency in get-tugas.ts

--- features/tugas/get-tugas.ts
+++ features/tugas/get-tugas.ts
@@ -1,7 +1,7 @@
-'use server';
+"use server";
 
-import { db } from '@/drizzle/server';
-import { tugas } from '@/drizzle/schema';
+import { db } from "@/drizzle/server";
+import { tugas } from "@/drizzle/schema";
 
 export async function getTugas() {
   const data = await db.select().from(tugas).limit(10);
