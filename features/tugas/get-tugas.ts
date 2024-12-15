249fe59ac654510dfd48c5d56f79e59a957e0dc9
rafadlis <rahmatfadlis@gmail.com>
2024-12-15 21:26:50 +0700

    refactor(db): adapt to fadf09f7209987e3df44178d3a14493c752b44cd changes

--- features/tugas/get-tugas.ts
+++ features/tugas/get-tugas.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { db } from "@/drizzle/server";
+import { db_drizzle } from "@/drizzle/server";
 import { Tugas } from "@/drizzle/schema";
 
 export async function getTugas() {
-  const data = await db.select().from(Tugas).limit(10);
+  const data = await db_drizzle.select().from(Tugas).limit(10);
   return data;
 }
