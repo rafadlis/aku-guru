fadf09f7209987e3df44178d3a14493c752b44cd
rafadlis <rahmatfadlis@gmail.com>
2024-12-15 21:26:02 +0700

    feat(db): add db connection via supabase-js

--- drizzle/server.ts
+++ drizzle/server.ts
@@ -1,10 +1,19 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-import * as relations from './relations';
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import * as relations from "./relations";
+import { createClient } from "@/supabase/server";
 
 const connectionString = process.env.DATABASE_URL;
 
-export const client = postgres(connectionString!, { prepare: false });
-export const db = drizzle(client, { schema: { ...schema, ...relations } });
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const client = postgres(connectionString, { prepare: false });
+export const db_drizzle = drizzle(client, {
+  schema: { ...schema, ...relations },
+});
+
+export const db_supabase = await createClient();
